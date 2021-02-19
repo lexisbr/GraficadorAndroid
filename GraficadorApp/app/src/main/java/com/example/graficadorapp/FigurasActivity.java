e14d45b27051ce944ae37e0d06159832f6218af0
lexisbr <josebarriosrodas@gmail.com>
2021-02-19 02:48:49 -0600

    Grafica figura exceptuando poligono

--- GraficadorApp/app/src/main/java/com/example/graficadorapp/FigurasActivity.java
+++ GraficadorApp/app/src/main/java/com/example/graficadorapp/FigurasActivity.java
@@ -1,19 +1,24 @@
 package com.example.graficadorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.text.style.TtsSpan;
 
+import com.example.graficadorapp.herramientas.Dibujador;
 import com.example.graficadorapp.lexer.Lexer;
 import com.example.graficadorapp.objetos.Figura;
+import com.example.graficadorapp.objetos.Graficador;
 import com.example.graficadorapp.parser.Parser;
 
 import java.io.StringReader;
 import java.util.ArrayList;
 
 public class FigurasActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +40,20 @@ public class FigurasActivity extends AppCompatActivity {
             } else {
                 //parser.getGraficador().mostrarFiguras();
                 ArrayList<Figura> listaFiguras  = parser.getGraficador().getListaFiguras();
+                ConstraintLayout layout =  (ConstraintLayout) findViewById(R.id.layoutFiguras);
+
+                Dibujador dibujador = new Dibujador(this,listaFiguras);
+                dibujador.mostrarFiguras();
+                layout.addView(dibujador);
+
             }
 
         } catch (Exception ex) {
             System.out.println("Error irrecuperable " + ex);
         }
 
-        /*TextView textView = findViewById(R.id.textView);
-        textView.setText(message);*/
+
+
     }
 
 
---
+++ GraficadorApp/app/src/main/java/com/example/graficadorapp/herramientas/Dibujador.java
@@ -0,0 +1,175 @@
+package com.example.graficadorapp.herramientas;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.view.View;
+
+import com.example.graficadorapp.objetos.Circulo;
+import com.example.graficadorapp.objetos.Cuadrado;
+import com.example.graficadorapp.objetos.Figura;
+import com.example.graficadorapp.objetos.Linea;
+import com.example.graficadorapp.objetos.Poligono;
+import com.example.graficadorapp.objetos.Rectangulo;
+
+import java.util.ArrayList;
+
+public class Dibujador extends View {
+    private ArrayList<Figura> listaFiguras;
+
+    public Dibujador(Context context, ArrayList<Figura> listaFiguras) {
+        super(context);
+        this.listaFiguras = listaFiguras;
+    }
+
+    public ArrayList<Figura> getListaFiguras() {
+        return listaFiguras;
+    }
+
+    protected void onDraw(Canvas canvas){
+        int[] rgbColor = new int[3];
+        /*Fondo del lienzo*/
+        canvas.drawRGB(204, 255, 255);
+
+        /*Pincel*/
+        Paint pincel = new Paint();
+
+
+        for(Figura figura: listaFiguras){
+            int x = figura.getPosicion_x();
+            int y = figura.getPosicion_y();
+            rgbColor = crearColor(figura.getColor());
+            pincel.setARGB(255, rgbColor[0], rgbColor[1], rgbColor[2]);
+            pincel.setStrokeWidth(1);
+
+
+
+            if(figura instanceof Cuadrado){
+                Cuadrado forma = (Cuadrado) figura;
+                canvas.drawRect(x,y,x+forma.getLongitud_lado(),y+forma.getLongitud_lado(),pincel);
+              /*  for(int i = 0; i <= forma.getLongitud_lado(); i++){
+                    canvas.drawLine(x, y+i, x+forma.getLongitud_lado(), y+i, pincel);
+                }*/
+            }else if(figura instanceof Circulo){
+                Circulo forma = (Circulo) figura;
+                canvas.drawCircle(x,y,forma.getRadio(),pincel);
+            }else if(figura instanceof Rectangulo){
+                Rectangulo forma = (Rectangulo) figura;
+                canvas.drawRect(x,y,forma.getAncho()+x,y+forma.getAlto(),pincel);
+            }else if (figura instanceof Linea){
+                pincel.setStrokeWidth(15);
+                Linea forma = (Linea) figura;
+                canvas.drawLine(x,y,forma.getPosicion_x2(),forma.getPosicion_y2(),pincel);
+            }
+
+        }
+
+    }
+
+    public int[] crearColor(String color){
+        int[] rgbColor = new int[3];
+        if(color.equals("azul")){
+            rgbColor[0] = 0;
+            rgbColor[1] = 102;
+            rgbColor[2] = 204;
+        }else if (color.equals("rojo")){
+            rgbColor[0] = 255;
+            rgbColor[1] = 0;
+            rgbColor[2] = 0;
+        }else if (color.equals("verde")){
+            rgbColor[0] = 0;
+            rgbColor[1] = 255;
+            rgbColor[2] = 0;
+        }else if (color.equals("amarillo")){
+            rgbColor[0] = 255;
+            rgbColor[1] = 255;
+            rgbColor[2] = 0;
+        }else if (color.equals("naranja")){
+            rgbColor[0] = 255;
+            rgbColor[1] = 165;
+            rgbColor[2] = 0;
+        }else if (color.equals("morado")){
+            rgbColor[0] = 128;
+            rgbColor[1] = 0;
+            rgbColor[2] = 128;
+        }else if (color.equals("cafe")){
+            rgbColor[0] = 139;
+            rgbColor[1] = 69;
+            rgbColor[2] = 19;
+        }else if (color.equals("negro")){
+            rgbColor[0] = 0;
+            rgbColor[1] = 0;
+            rgbColor[2] = 0;
+        }
+        return rgbColor;
+    }
+
+    public void mostrarFiguras() {
+
+        for (int i = 0; i < listaFiguras.size(); i++) {
+            Figura figura = listaFiguras.get(i);
+
+            if (figura instanceof Circulo) {
+
+                Circulo forma = (Circulo) figura;
+                System.out.println("Se obtuvo un circulo " + forma.getColor() + " Radio: " + forma.getRadio());
+                if (forma.getAnimacion() == null) {
+                    System.out.println("No tiene animacion");
+                } else {
+                    System.out.println("Animacion " + forma.getAnimacion().getTipo_animacion());
+                }
+
+            } else if (figura instanceof Cuadrado) {
+
+                Cuadrado forma = (Cuadrado) figura;
+                System.out.println("Se obtuvo un cuadrado " + forma.getColor() + " Longitud de lados: " + forma.getLongitud_lado());
+                if (forma.getAnimacion() == null) {
+                    System.out.println("No tiene animacion");
+                } else {
+                    System.out.println("Animacion " + forma.getAnimacion().getTipo_animacion());
+
+                }
+
+            }else if (figura instanceof Rectangulo) {
+
+                Rectangulo forma = (Rectangulo) figura;
+                System.out.println("Se obtuvo un rectangulo " + forma.getColor() + " Alto: " + forma.getAlto()+" Ancho: "+forma.getAncho());
+                if (forma.getAnimacion() == null) {
+                    System.out.println("No tiene animacion");
+                } else {
+                    System.out.println("Animacion " + forma.getAnimacion().getTipo_animacion());
+
+                }
+
+            }else if (figura instanceof Linea) {
+
+                Linea forma = (Linea) figura;
+                System.out.println("Se obtuvo un linea " + forma.getColor() + " Pos x2: " + forma.getPosicion_x2()+" Pos y2: "+forma.getPosicion_y2());
+                if (forma.getAnimacion() == null) {
+                    System.out.println("No tiene animacion");
+                } else {
+                    System.out.println("Animacion " + forma.getAnimacion().getTipo_animacion());
+
+                }
+
+            }else if (figura instanceof Poligono) {
+
+                Poligono forma = (Poligono) figura;
+                System.out.println("Se obtuvo un poligono " + forma.getColor() + " Cantidad de lados: " + forma.getCantidad_lados());
+                if (forma.getAnimacion() == null) {
+                    System.out.println("No tiene animacion");
+                } else {
+                    System.out.println("Animacion " + forma.getAnimacion().getTipo_animacion());
+
+                }
+
+            }
+
+
+        }
+    }
+
+
+}
